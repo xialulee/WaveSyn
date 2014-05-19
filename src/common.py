de68caab7d059d74b253c9af8d7641f5d13d8805
xialulee <xialulee@sina.com>
2014-05-19 11:01:07 +0800

    New formatter for string formating.

--- src/common.py
+++ src/common.py
@@ -6,7 +6,7 @@ Created on Sat May 17 20:05:23 2014
 """
 
 import sys
-from string import Template
+from string import Template, Formatter
 
 class MethodDelegator(object):
     def __init__(self, attrName, methodName)   :
@@ -24,3 +24,24 @@ def setMultiAttr(obj, **kwargs):
         
 def autoSubs(template):
     return Template(template).substitute(sys._getframe(1).f_locals)
+    
+    
+class AdvancedFormatter(Formatter):
+    def __init__(self, *args, **kwargs):
+        Formatter.__init__(self, *args, **kwargs)
+        self.caller = None # Will be set by method "format".
+                
+#    def check_unused_args(self, used_args, args, kwargs):
+#        return        
+        
+    def get_value(self, expr, args=None, kwargs=None):
+        caller   = self.caller        
+        return eval(expr, caller.f_globals, caller.f_locals)
+        
+    def get_field(self, field_name, args, kwargs):
+        obj = self.get_value(field_name, args, kwargs)
+        return obj, field_name        
+        
+    def format(self, format_string, *args, **kwargs):
+        self.caller = sys._getframe(1)
+        return Formatter.format(self, format_string, *args, **kwargs)
