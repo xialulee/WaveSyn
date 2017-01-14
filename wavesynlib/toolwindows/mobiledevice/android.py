64c2f7275755a2ced4f9ba68230fe6bb3528645e
xialulee <xialulee@sina.com>
2017-01-14 23:11:24 +0800

    Refined Android data transfer.

--- wavesynlib/toolwindows/mobiledevice/android.py
+++ wavesynlib/toolwindows/mobiledevice/android.py
@@ -19,8 +19,9 @@ from PIL import ImageTk
 import six.moves.tkinter as tk
 import six.moves.tkinter_ttk as ttk
 from wavesynlib.toolwindows.basewindow import TkToolWindow
-from wavesynlib.guicomponents.tk import json_to_tk, ScrolledCanvas
+from wavesynlib.guicomponents.tk import json_to_tk, ScrolledCanvas, ScrolledText
 from wavesynlib.interfaces.timer.tk import TkTimer
+from wavesynlib.languagecenter.wavesynscript import Scripting, code_printer
 
 class DataTransferWindow(TkToolWindow):
     window_name = 'WaveSyn-DataTransfer'
@@ -35,9 +36,9 @@ data_type: text / image
         tool_tabs = self._tool_tabs
         
         widgets_desc = [
-{'class':'Group', 'pack':{'side':'left', 'fill':'y'}, 'setattr':{'name':'Clipboard Sync'}, 'children':[
-    {'class':'Button', 'config':{'text':'From Dev', 'command':lambda:self._launch_server({'direction':'from device', 'data_type':'text', 'clipboard':True})}},
-    {'class':'Button', 'config':{'text':'To Dev', 'command':lambda:self._launch_server({'direction':'to device', 'data_type':'text', 'clipboard':True})}}
+{'class':'Group', 'pack':{'side':'left', 'fill':'y'}, 'setattr':{'name':'Clipboard'}, 'children':[
+    {'class':'Button', 'config':{'text':'Read', 'command':self.__on_read_device_clipboard}},
+    {'class':'Button', 'config':{'text':'Write', 'command':lambda:self._launch_server({'direction':'to device', 'data_type':'text', 'clipboard':True})}}
 ]}
 ]
 
@@ -53,22 +54,39 @@ data_type: text / image
         self.__qr_id = None
         self.__password = None
         self.__data = None
-        self.__finish_event = threading.Event()
-
-        self.__timer = TkTimer(self, interval=100)
-        self.__timer.active = False
-        self.__timer.add_observer
-        def observer():
-            pass
+        self.__busy = threading.Event()
+        self.__conn = threading.Event()
+        self.__on_finish = None
 
         data_book.add(qr_canvas, text='QR Code')
+        
+        self.__scrolled_text = scrolled_text = ScrolledText(data_book)
+        data_book.add(scrolled_text, text='Text')
+        
         data_book.pack(expand='yes', fill='both')
         
         self._make_window_manager_tab()
         
+        self.__timer = TkTimer(tk_object, interval=100)
+        self.__timer.active = False
+        @self.__timer.add_observer
+        def observer():
+            if self.__conn.is_set():
+                self.__conn.clear()
+                self.__qr_canvas.canvas.delete(self.__qr_id)
+                self.__qr_id = None
+            
+            if not self.__busy.is_set(): # Finished
+                self.__timer.active = False
+                if self.device_data['type'] == 'text':
+                    scrolled_text.set_text(self.device_data['data'].encode('utf-8'))
+                if self.__on_finish:
+                    self.__on_finish(self.device_data['data'])
+                    self.__on_finish = None
+        
         
     @property
-    def data(self):
+    def device_data(self):
         return self.__data
         
         
@@ -99,12 +117,14 @@ data_type: text / image
             self.__qr_canvas.canvas.itemconfig(self.__qr_id, image=self.__qr_image)  
             
         thread.start_new_thread(self._server_thread, (sockobj, command))
-        self.__finish_event.clear()
+        self.__busy.set()
+        self.__timer.active = True
     
     
     def _server_thread(self, sockobj, command):
         sockobj.listen(2)
         conn, addr = sockobj.accept()
+        self.__conn.set()
 
         exit_flag = conn.recv(1)
         if exit_flag != b'\x00':
@@ -122,12 +142,19 @@ data_type: text / image
                 data_list.append(data)
             data = b''.join(data_list)
             if command['data_type'] == 'text':
-                self.__data = data.decode('utf-8')
-            self.__finish_event.set()
+                self.__data = {'data':data.decode('utf-8'), 'type':'text'}
+            self.__busy.clear()
+        
+    
+    @Scripting.printable
+    def read_device_clipboard(self, on_finish):
+        self._launch_server({'direction':'from device', 'data_type':'text', 'clipboard':True})
+        self.__on_finish = on_finish
         
         
-    def _clipb_from_dev(self):
-        pass
+    def __on_read_device_clipboard(self, on_finish=None):
+        with code_printer:
+            self.read_device_clipboard(on_finish=on_finish)
     
     
     def _clipb_to_dev(self):
