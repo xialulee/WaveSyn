6f70f8bbe046d7fc06b7eff6c3a053e7fa4911fd
xialulee <xialulee@sina.com>
2017-01-15 23:49:43 +0800

    Code for reading android device location.

--- wavesynlib/toolwindows/mobiledevice/datatransferclient.py
+++ wavesynlib/toolwindows/mobiledevice/datatransferclient.py
@@ -8,9 +8,12 @@ Created on Sat Jan 14 19:38:45 2017
 import socket
 import json
 import struct
-import androidhelper
+import time
 
-droid = androidhelper.Android()
+import androidhelper as android
+
+droid = android.Android()
+
 
 def read_qr():
     result = droid.startActivityForResult('com.google.zxing.client.android.SCAN', extras={'SAVE_HISTORY':False})
@@ -20,18 +23,47 @@ def read_qr():
     password = data['password']
     command = data['command']
     return ip, port, password, command
-
-def send_clipb_text(ip, port, password):
-    text = droid.getClipboard().result.encode('utf-8')
+    
+    
+def send_text(text, ip, port, password):
     sockobj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sockobj.connect((ip, port))
     sockobj.send('\x00')
     sockobj.send(struct.pack('!I', password))
     sockobj.send(text)
     sockobj.close()
+    
+    
+def send_clipb_text(ip, port, password):
+    text = droid.getClipboard().result.encode('utf-8')
+    send_text(text, ip, port, password)
+    
+    
+def get_location(interval):
+    droid.startLocating()
+    time.sleep(interval)
+    location = droid.readLocation().result
+    
+    if not location:
+        location = droid.getLastKnownLocation()
+    
+    if 'gps' in location:
+        data = location['gps']
+    else:
+        data = location['network']
+        
+    return json.dumps({'longitude':data['longitude'], 'latitude':data['latitude']}, indent=2, separators=(',', ': '))
+    
+    
+def send_location_json(ip, port, password, interval=3):
+    text = get_location(interval)
+    send_text(text, ip, port, password)
+
 
 if __name__ == '__main__':
     ip, port, password, command = read_qr()
-    if command[u'direction'] == u'from device' and \
-        command[u'clipboard'] is True:
-        send_clipb_text(ip, port, password)
+    if command[u'direction'] == u'from device':
+        if 'clipboard' in command and command[u'clipboard'] is True:
+            send_clipb_text(ip, port, password)
+        elif 'location' in command and command[u'location'] is True:
+            location = get_location()
