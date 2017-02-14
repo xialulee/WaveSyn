71fbd9b3f2c23ec5183d39c70496f799c85cc849
xialulee <xialulee@sina.com>
2017-02-14 14:17:50 +0800

    Refined the HTML edit window.

--- wavesynlib/toolwindows/clipboard/cfhtmledit.py
+++ wavesynlib/toolwindows/clipboard/cfhtmledit.py
@@ -13,15 +13,20 @@ class CodeFrame(wx.Frame):
     def __init__(self, parent=None, id=-1, pos=wx.DefaultPosition, title='CF_HTML Edit', size=(800, 600)):        
         wx.Frame.__init__(self, parent, id, title, pos, size)
         sizer = wx.GridSizer(rows=1, cols=2)
-        code_text = wx.TextCtrl(self, -1, "Input HTML code here.", style=wx.TE_MULTILINE)
+        code_text = self.__code_text = wx.TextCtrl(self, -1, "Input HTML code here.", style=wx.TE_MULTILINE)
         code_text.SetInsertionPoint(0)
+        code_text.Bind(wx.EVT_KEY_UP, self.on_char)
         sizer.Add(code_text, 0, flag=wx.EXPAND)
         
-        code_html = wx.html.HtmlWindow(self)
-        code_html.SetPage('<hr>test<hr>')
+        code_html = self.__code_html = wx.html.HtmlWindow(self)
         sizer.Add(code_html, 1, flag=wx.EXPAND)
         self.SetSizer(sizer)
-#        self.Fit()
+        
+        
+    def on_char(self, evt):
+        html_code = self.__code_text.GetValue()
+        self.__code_html.SetPage(html_code)
+        
         
 
 class App(wx.App):
