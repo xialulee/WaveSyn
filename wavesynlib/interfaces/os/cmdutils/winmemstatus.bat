3be59c6e3a31957e1e9a3e9a5ca2fd01617a1eb6
xialulee <xialulee@sina.com>
2017-02-22 21:35:04 +0800

    New menu item for launching RAM meter.

---
+++ wavesynlib/interfaces/os/cmdutils/winmemstatus.bat
@@ -0,0 +1,6 @@
+@rem The directory of this bat file:
+@rem   wavesynlib\interfaces\cmdutils
+@rem The directory of memstatus.py:
+@rem   wavesynlib\interfaces\windows\
+
+%~dp0\..\windows\memstatus.pyw
