575236868ee79a64b6cc0e1551e8caaa5371f8f2
xialulee <xialulee@sina.com>
2017-02-27 16:22:40 +0800

    Bug fix.

--- wavesynlib/interfaces/os/cmdutils/clipbcfhtml.bat
+++ wavesynlib/interfaces/os/cmdutils/clipbcfhtml.bat
@@ -1,12 +1,12 @@
 @echo off
 
 rem The directory of this bat file:
-rem   wavesynlib\interfaces\windows\cmdutils
+rem   wavesynlib\interfaces\os\cmdutils
 rem The directory of clipb.py:
-rem   wavesynlib\interfaces\windows\clipboard
+rem   wavesynlib\interfaces\os\windows\clipboard
 
 if "%1"=="--help" (
   echo Convert HTML code in clipboard into formatted text in CF_HTML.
 ) else (
-  %~dp0\..\clipboard\clipb.py -r -e@ | %~dp0\..\clipboard\clipb.py -w -d@ --html
+  %~dp0\..\windows\clipboard\clipb.py -r -e@ | %~dp0\..\windows\clipboard\clipb.py -w -d@ --html
 )
