728449beccb381ac5c5ec1e79725193c60656b86
xialulee <xialulee@sina.com>
2017-03-02 22:26:49 +0800

    wswql.py supports json output.

--- wavesynlib/interfaces/os/cmdutils/wswql.py
+++ wavesynlib/interfaces/os/cmdutils/wswql.py
@@ -8,13 +8,30 @@ Created on Thu Mar 02 15:54:06 2017
 from __future__ import print_function, division, unicode_literals
 
 import sys
+import getopt
 from comtypes import client
 
 from wavesynlib.languagecenter.datatypes import Table
 
 
 def main(argv):
-    wql_str = argv[1]
+    try:
+        opts, args = getopt.getopt(argv[1:], \
+            '',\
+            ['jsontable']\
+        ) # TODO
+    except getopt.GetoptError, err:
+        print(str(err), file=sys.stderr)
+        #usage()
+        return 1
+        
+    json_output = False
+    for o, a in opts:
+        if o == '--jsontable':
+            json_output = True      
+        
+    
+    wql_str = args[0]
     loc = client.CreateObject('WbemScripting.SWbemLocator')
     svc = loc.ConnectServer('.', 'root\\cimv2')
     items = svc.ExecQuery(wql_str)
@@ -32,9 +49,10 @@ def main(argv):
         if flag:
             table.head = head
             flag = False
-            table.print_head()
-            print('-'*25)
-        table.print_row(row, lang='plain')
+            if not json_output:
+                table.print_head()
+                print('-'*25)
+        table.print_row(row, lang='json' if json_output else 'plain')
             
             
 if __name__ == '__main__':
