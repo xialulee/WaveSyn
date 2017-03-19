cfce24e292ecbc9aae8a36d0e0570d602bd952ff
xialulee <xialulee@sina.com>
2017-03-19 22:25:22 +0800

    Add a copy button to the cfhtmledit window.

--- wavesynlib/toolwindows/clipboard/cfhtmledit.py
+++ wavesynlib/toolwindows/clipboard/cfhtmledit.py
@@ -14,16 +14,23 @@ class CodeFrame(wx.Frame):
         wx.Frame.__init__(self, parent, id, title, pos, size)
         sizer = wx.GridSizer(rows=1, cols=2)
         panel_left = wx.Panel(self, -1)
+        panel_bottom = wx.Panel(panel_left, -1)
 
         vbox_left = wx.BoxSizer(wx.VERTICAL)
+        hbox_bottom = wx.BoxSizer(wx.HORIZONTAL)
         code_text = self.__code_text = wx.TextCtrl(panel_left, -1, "Input HTML code here.", style=wx.TE_MULTILINE)
         code_text.SetInsertionPoint(0)
         code_text.Bind(wx.EVT_KEY_UP, self.on_char)
-        button_clear = wx.Button(panel_left, -1, 'Clear')
+        
+        button_clear = wx.Button(panel_bottom, -1, 'Clear')
         self.Bind(wx.EVT_BUTTON, self.on_clear_click, button_clear)
+        button_copy = wx.Button(panel_bottom, -1, 'Copy')
+        hbox_bottom.Add(button_copy, 1)
+        hbox_bottom.Add(button_clear, 1)
+        panel_bottom.SetSizer(hbox_bottom)
                 
         vbox_left.Add(code_text, 1, wx.EXPAND)
-        vbox_left.Add(button_clear)
+        vbox_left.Add(panel_bottom)
                 
         panel_left.SetSizer(vbox_left)
         sizer.Add(panel_left, 0, flag=wx.EXPAND)
