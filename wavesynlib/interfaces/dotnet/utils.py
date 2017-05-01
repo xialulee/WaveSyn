6b3564d7b4a3d7f22970822dc6f149be4eacdf4b
xialulee <xialulee@sina.com>
2017-05-01 23:09:55 +0800

    New function convert dotnet Bitmap to numpy array.

--- wavesynlib/interfaces/dotnet/utils.py
+++ wavesynlib/interfaces/dotnet/utils.py
@@ -7,7 +7,9 @@ Created on Mon Apr 24 22:57:24 2017
 
 from __future__ import print_function, division, unicode_literals
 
+import six
 import clr
+import System
 
 
 
@@ -38,11 +40,30 @@ def new_and_init(class_object, **kwargs):
     return obj
 
         
+
+class BitmapUtils(object):
+    @staticmethod
+    def pil_to_net(image):    
+        from System.Drawing import Bitmap    
+        ns = NETMemoryStream()
+        image.save(ns, 'png')
+        ns.seek(0)
+        return Bitmap(ns.net_object)
         
-def pil_to_net(image):    
-    from System.Drawing import Bitmap    
-    ns = NETMemoryStream()
-    image.save(ns, 'png')
-    ns.seek(0)
-    return Bitmap(ns.net_object)  
+        
+    @staticmethod
+    def net_to_matrix(bitmap):
+        from System.IO import MemoryStream
+        from System.Drawing.Imaging import ImageFormat
+        ms = MemoryStream()
+        bitmap.Save(ms, ImageFormat.Png)
+        arr = ms.ToArray()
+        from cStringIO import StringIO
+        stream = StringIO()
+        stream.write(six.binary_type('').join([chr(c) for c in arr]))
+        from scipy.ndimage import imread
+        return imread(stream)
+        
+    
+    
         
--- wavesynlib/interfaces/dotnet/zxing/__init__.py
+++ wavesynlib/interfaces/dotnet/zxing/__init__.py
@@ -10,7 +10,7 @@ import os
 
 from wavesynlib.languagecenter.wavesynscript import ModelNode, Scripting
 from wavesynlib.languagecenter.utils import get_caller_dir
-from wavesynlib.interfaces.dotnet.utils import pil_to_net, new_and_init
+from wavesynlib.interfaces.dotnet.utils import new_and_init, BitmapUtils
 
 import clr
 clr.AddReference(os.path.join(get_caller_dir(), 'zxing.dll'))
@@ -29,7 +29,7 @@ class ZXingNET(ModelNode):
     def read(self, image):
         image = self.root_node.interfaces.os.clipboard.support_clipboard_image(image)
         # To Do: support ask open file dialog
-        image = pil_to_net(image)
+        image = BitmapUtils.pil_to_net(image)
         result = BarcodeReader().Decode(image)
         if result is None:
             raise ValueError('Input image seems not contain any barcode.')
