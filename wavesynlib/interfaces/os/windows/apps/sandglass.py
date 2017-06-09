0e7bed1252b62f430808dbe1cfe7124c5e2f7d74
xialulee <xialulee@sina.com>
2017-06-10 00:07:55 +0800

    New sandglass app.

---
+++ wavesynlib/interfaces/os/windows/apps/sandglass.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 09 22:58:03 2017
+
+@author: Feng-cong Li
+"""
+from __future__ import division, print_function, unicode_literals
+
+import sys
+from six.moves.tkinter import *
+import ctypes as ct
+import time
+
+from wavesynlib.guicomponents import tk as tktools
+from wavesynlib.interfaces.timer.tk import TkTimer
+from wavesynlib.interfaces.os.windows.shell.constants import TBPFLAG
+
+APPID = u'548CEA05-47A1-455C-9332-11F6560865F3'
+
+
+
+def main(argv):
+    ct.windll.shell32.SetCurrentProcessExplicitAppUserModelID(APPID+str(time.time()))
+    root = Tk()
+    label = Label(root)
+    label.pack()
+    tb_icon = tktools.TaskbarIcon(root)
+    
+    total = int(argv[1])
+    current = [0]
+    
+    timer = TkTimer(widget=root, interval=1000)
+    
+    @timer.add_observer
+    def show_left():
+        current[0] += 1
+        percent = int(current[0] / total * 100)
+        tb_icon.progress = percent
+        label['text'] = '{} seconds left.'.format(total-current[0])
+        if 0 <= percent <= 50:
+            state = TBPFLAG.TBPF_NORMAL
+        elif 50 < percent <= 80:
+            state = TBPFLAG.TBPF_PAUSED
+        else:
+            state = TBPFLAG.TBPF_ERROR
+        tb_icon.state = state
+        if current[0] >= total:
+            timer.active = False
+            tb_icon.state = TBPFLAG.TBPF_NOPROGRESS
+        
+    timer.active = True
+    root.mainloop()
+    
+    
+    
+if __name__ == '__main__':
+    main(sys.argv)
--- wavesynlib/interfaces/os/windows/modelnode.py
+++ wavesynlib/interfaces/os/windows/modelnode.py
@@ -8,7 +8,7 @@ Created on Thu Mar 02 22:41:20 2017
 from __future__ import print_function, division, unicode_literals
 
 import os
-import webbrowser
+from subprocess import Popen
 
 from wavesynlib.languagecenter.utils import get_caller_dir
 from wavesynlib.languagecenter.wavesynscript import Scripting, ModelNode
@@ -40,12 +40,17 @@ class Windows(ModelNode):
         
     
     @Scripting.printable
-    def launch(self, app_name):
+    def launch(self, app_name, *args):
         if app_name in app_paths:
             app_path = app_paths[app_name]
         else:
             self_dir = get_caller_dir()
             app_path = os.path.join(self_dir, 'apps', app_name)
-        webbrowser.open(app_path)
+        
+        cmd = ['python', app_path]
+        if args:
+            args = [str(arg) for arg in args]
+        cmd.extend(args)
+        Popen(cmd)
         
         
