4c2bf01ffad913f0b36750d4fc5c320b4e14aad4
xialulee <xialulee@sina.com>
2017-08-28 01:24:39 +0800

    Refined markdown table generator implementation.
    Now markdown table generator supports generating tables from html code
    in clipboard. Only windows platform is supported.

--- wavesynlib/languagecenter/markdown/modelnode.py
+++ wavesynlib/languagecenter/markdown/modelnode.py
@@ -13,9 +13,19 @@ from wavesynlib.languagecenter.markdown import utils
 
 class Utils(ModelNode):
     def __init__(self, *args, **kwargs):
-        super(Utils, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
         
         
     @Scripting.printable
     def generate_table(self, table):
-        return utils.generate_table(table)
+        if hasattr(self.root_node.interfaces.os.clipboard, 'support_clipboard_html'):
+            if table is self.root_node.lang_center.wavesynscript.constants.CLIPBOARD_HTML:                
+                tables = self.root_node.lang_center.html_utils.get_tables(table)
+                ret = []
+                for table in tables:
+                    ret.append(utils.generate_table(table))
+                return '\n\n'.join(ret)
+            else:
+                return utils.generate_table(table)
+        else:
+            return utils.generate_table(table)
--- wavesynlib/languagecenter/markdown/utils.py
+++ wavesynlib/languagecenter/markdown/utils.py
@@ -11,10 +11,10 @@ from __future__ import print_function, division, unicode_literals
 
 def generate_table(table):
     head = table[0]
-    head_code = ''.join(('|'+item for item in head))
+    head_code = ''.join(('|'+str(item) for item in head))
     split = '|'.join(['---']*len(head))
     rows = [head_code, split]
     for row in table[1:]:
-        rows.append('|'.join(row))
+        rows.append('|'.join([str(item) for item in row]))
     return '\n'.join(rows)
     
