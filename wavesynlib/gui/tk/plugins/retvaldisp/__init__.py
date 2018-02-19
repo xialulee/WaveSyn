9d3e1545b7c23569a1453a8b2b1ef6dd222d103e
xialulee <xialulee@sina.com>
2018-02-20 01:18:50 +0800

    New plugin system for console to display extra information/link for specific return value types.

---
+++ wavesynlib/gui/tk/plugins/retvaldisp/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 07 17:08:49 2017
+
+@author: Feng-cong Li
+"""
+
---
+++ wavesynlib/gui/tk/plugins/retvaldisp/pathdisp.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 20 01:09:40 2018
+
+@author: Feng-cong Li
+"""
+
+from pathlib import Path
+
+
+
+class Plugin:
+    _type = Path
+    
+    def __init__(self, root_node):
+        self.__root = root_node
+        
+        
+    def test_data(self, data):
+        if isinstance(data, self._type):
+            return True
+        else:
+            return False
+        
+        
+    def action(self, data):
+        if self.test_data(data):
+            self.__root.print_tip([{'type':'file_list', 'content':(str(data),)}])
+            
