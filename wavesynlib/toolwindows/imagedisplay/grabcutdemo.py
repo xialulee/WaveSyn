4c54a21d57cd7dc835391207e28b1b42bb77e4e6
xialulee <xialulee@sina.com>
2018-04-21 23:54:29 +0800

    Modified grabcutdemo.

--- wavesynlib/toolwindows/imagedisplay/grabcutdemo.py
+++ wavesynlib/toolwindows/imagedisplay/grabcutdemo.py
@@ -10,6 +10,7 @@ from tkinter import Tk
 import pylab
 import numpy as np
 import cv2
+import time
 
 
 # see https://stackoverflow.com/questions/38048650/opencv-grabcut-background-color-and-contour-in-python
@@ -24,7 +25,10 @@ if __name__ == '__main__':
     
     def callback(rect, dumb):
         mask = np.zeros(im.shape[:2], np.uint8)
+        t_0 = time.time()
         cv2.grabCut(im, mask, rect, None, None, 6, cv2.GC_INIT_WITH_RECT)
+        deltaT = time.time() - t_0
+        print(f'deltaT = {deltaT}')
         mask = np.where((mask==2)|(mask==0), 0, 1).astype('uint8')
         img = im * mask[:,:, np.newaxis]
         pylab.imshow(img)
