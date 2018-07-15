045748f5809e964a47462d48d4180c362b996afb
xialulee <xialulee@sina.com>
2018-07-15 17:38:17 +0800

    Start the unrar interface developing.

---
+++ wavesynlib/interfaces/unrar/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 15 17:27:43 2018
+
+@author: Feng-cong Li
+"""
+
---
+++ wavesynlib/interfaces/unrar/modelnode.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 15 17:28:37 2018
+
+@author: Feng-cong Li
+"""
+
+import subprocess as sp
+
+
+
+def test(path):
+    p = sp.Popen(['unrar', 'lta', path], stdout=sp.PIPE, stderr=sp.PIPE)
+    
