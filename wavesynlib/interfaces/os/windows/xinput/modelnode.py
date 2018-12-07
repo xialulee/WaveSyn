7dff3e3f959057f15b7f279f880c826be070d88c
xialulee <xialulee@live.cn>
2018-12-07 22:23:17 +0800

    Refined xinput interface implementation.

--- wavesynlib/interfaces/os/windows/xinput/modelnode.py
+++ wavesynlib/interfaces/os/windows/xinput/modelnode.py
@@ -4,15 +4,24 @@ Created on Wed Dec  5 20:12:50 2018
 
 @author: Feng-cong Li
 """
+import _thread as thread
 
 from wavesynlib.languagecenter.wavesynscript import ModelNode
-from wavesynlib.interfaces.os.windows.xinput import utils
+from wavesynlib.interfaces.os.windows.xinput import utils, constants
 
 
 
-class Gamepad(ModelNode):
+class Gamepads(ModelNode):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.__op_map = [{}, {}, {}, {}]
+        self.__button_names = tuple(
+            name[len('XINPUT_GAMEPAD_'):].lower() \
+                for name in dir(constants) \
+                if name.startswith('XINPUT_GAMEPAD_') )
+        self.__stop_flag = [True]
+        self.__lock = thread.allocate_lock()
+        self.__button_state = [[], [], [], []]
         
         
     def enable(self, en):
@@ -31,6 +40,54 @@ Valid motor usage percent should be in [0, 100].''')
             p2s(right_motor_percent))
         
         
+    def get_availability(self):
+        return [self.is_available(uid) for uid in range(4)]
+    
+    
+    def get_button_names(self):
+        return self.__button_names
+    
+    
+    def set_button_map(self, user_index, button, type_, action):
+        '''\
+Map the specified button to an action.
+
+user_index: the ID of the gamepad;
+button: the name of the button (call get_button_names to get available names);
+type_: "press" or "release", indicating the action trigger by button press or
+    button release;
+action: the action trigger by the gamepad button, action can be a string
+    indicating a name of the keyboard, a list indicating a chain events or
+    a callable object.
+'''
+        if not callable(action):
+            if isinstance(action, dict):
+                if action['type'].lower() == 'key':
+                    key_name = action['name']
+                    if 'modifiers' in action:
+                        mods = action['modifiers']
+                    else:
+                        mods = None
+                    kwargs = {'modifiers':mods}
+                    if type_ == 'press':
+                        kwargs['press'] = True
+                    elif type_ == 'release':
+                        kwargs['release'] = True
+                    def action():
+                        self.root_node.interfaces.os.windows.\
+                        input_senders.key_sender.send(key_name, **kwargs)
+
+        with self.__lock:
+            if button in self.__op_map[user_index]:
+                self.__op_map[user_index][button][type_] = action
+            else:
+                self.__op_map[user_index][button] = {type_:action}
+            
+            
+    def get_button_map(self):
+        return self.__op_map
+        
+        
     def is_available(self, user_index):
         try:
             utils.get_state(user_index)
@@ -45,10 +102,70 @@ Valid motor usage percent should be in [0, 100].''')
         
     def get_battery_info(self, user_index):
         return utils.get_battery_info(user_index)
-
+    
+    
+    def start_listening(self):
+        if not self.__stop_flag[0]:
+            return
+        self.__stop_flag[0] = False
+        
+        lock = self.__lock
+        
+        @self.root_node.thread_manager.new_thread_do
+        def listen():
+            packet_number = -1
+            current = [set(), set(), set(), set()]
+            
+            while True:
+                with lock:
+                    if self.__stop_flag[0]:
+                        break
+                    
+                availability = self.get_availability()
+                for uid in range(4):
+                    if not availability[uid]:
+                        continue
+                    
+                    state = utils.get_state(uid)
+                    if packet_number == state.dwPacketNumber:
+                        continue
+                    else:
+                        packet_number = state.dwPacketNumber
+                        
+                    buttons = state.Gamepad.wButtons
+                    
+                    with lock:     
+                        names = self.__op_map[uid]                               
+                        for name in names:
+                            act_info = names[name]
+                            constant = getattr(
+                                constants,
+                                f'XINPUT_GAMEPAD_{name.upper()}').value
+                            if constant & buttons:
+                                if name not in current[uid]: # Pressed
+                                    current[uid].add(name)
+                                    if 'press' in act_info:
+                                        action = act_info['press']
+                                        self.root_node.\
+                                        thread_manager.\
+                                        main_thread_do(block=False)(action)
+                            else:
+                                if name in current[uid]: # Released
+                                    current[uid].remove(name)
+                                    if 'release' in act_info:
+                                        action = act_info['release']
+                                        self.root_node.\
+                                        thread_manager.\
+                                        main_thread_do(block=False)(action)
+    
+    
+    def stop_listening(self):
+        with self.__lock:
+            self.__stop_flag[0] = True
+    
 
 
 class XInput(ModelNode):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.gamepad = Gamepad()        
+        self.gamepads = Gamepads()        
--- wavesynlib/languagecenter/hy/cdef.hy
+++ wavesynlib/languagecenter/hy/cdef.hy
@@ -1,70 +1,85 @@
-;(defmacro compound [typename name fields]
-;    `(defclass ~name [~typename]
-;        [-fields- 
-;	    ~(lfor i (range 0 (len fields) 2)
-;	        `(, 
-;		    ~(str (get fields (+ i 1)))
-;		    ~(get fields i)))]))
 
 
-(defmacro compound [typename name fields]
+(defmacro compound [type-name name fields]
     (setv class-fields [])
-    (if-not (symbol? typename) (do ;if not symbol, should be a list.
-        (for [k (range 1 (len typename) 2)]
-            (if (= (str (nth typename k)) "pack")
-                (class-fields.extend ['-pack- (nth typename (inc k))])))
-        (setv typename (first typename))))
+    (if (coll? type-name) (do ;if not symbol, should be a list.
+        (for [k (range 1 (len type-name) 2)]
+            (if (= (str (nth type-name k)) "pack")
+                (class-fields.extend ['-pack- (nth type-name (inc k))])))
+        (setv type-name (first type-name))))
     (setv field-list [])
     (setv anonymous-list [])
     (for [k (range 0 (len fields) 2)]
         (setv type-obj (nth fields k))
         (setv field-name (-> (nth fields (inc k)) (str) (mangle)))
         (setv field-width 0)
-        (if-not (symbol? type-obj) (do
+        (if (coll? type-obj) (do
             ; if not symbol, should be a list or expression.
-            (if (= (type type-obj) (type '())) ; a expression.
+            (if (= (type type-obj) (type '())) 
+	        ; An expression.
                 (setv type-obj `(~@type-obj))
             (do ; else a list
                 (if (= "anonymous" (str (first type-obj))) (do
                     (anonymous-list.append field-name)
                     (type-obj.pop 0)))
                 (if (instance? int (last type-obj))
+		    ; Bit field. 
                     (setv field-width (last type-obj)))
                 (setv type-obj (first type-obj))))))
             (setv field-desc [field-name type-obj])
         (if field-width (field-desc.append field-width))
+	; ctypes.Structure/Union only supports field descriptions
+	; in tuple type. 
         (field-list.append `(tuple ~field-desc)))
     (if anonymous-list 
         (class-fields.extend ['-anonymous- `(tuple ~anonymous-list)]))
-    (class-fields.append '-fields-)
-    (class-fields.append field-list)
-    `(defclass ~name [~typename] ~class-fields))
+    (class-fields.extend ['-fields- field-list])
+    `(defclass ~name [~type-name] ~class-fields))
 
 ;; Examples
-;(import [ctypes [*]])
+; import [ctypes [*]])
 ;
 ;; Basic Usage
-;(compound Structure Point [
+; compound Structure Point [
 ;    c_int x
 ;    c_int y])
 ;
 ;; Setting _pack_
-;(compound [Structure pack 32] Point [
+; compound [Structure pack 32] Point [
 ;    c_int x
 ;    c_int y])
 ;
 ;; Bit fields
-;(compound Structure Int [
+; compound Structure Int [
 ;    [c_int 16] first-16
 ;    [c_int 16] second-16])
 ;
 ;; Anonymous
-;(compound Union -U [
+; compound Union -U [
 ;    (POINTER TYPEDESC) lptdesc
 ;    (POINTER ARRAYDESC) lpadesc
 ;    HREFTYPE hreftype])
 ;
-;(compound Structure TYPEDESC [
+; compound Structure TYPEDESC [
 ;    [anonymous -U] u
 ;    VARTYPE vt])
+
+
+
+(defmacro funcptr [type-name res-type name arg-types]
+    (if (= res-type 'void)
+        (setv res-type None))
+    `(setv ~name 
+        (~type-name ~res-type ~@arg-types)))
+
+;;Example
+; (import [ctypes [*]])
+; 
+; (funcptr CFUNCTYPE 
+;     c_int CMPFUNC [(POINTER c_int) (POINTER c_int)])
+;
+; (qsort ia (len ia) (sizeof c_int) 
+;     (CMPFUNC (fn [a b]
+;         (print "hy_cmp_func" (first a) (first b))
+;         0)))
 
