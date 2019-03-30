023b4158292512e5549de32ac72cca475b204b57
xialulee <xialulee@live.cn>
2019-03-30 18:33:48 +0800

    Refined pydht11 implementation.

--- wavesynlib/cores/delayer.py
+++ wavesynlib/cores/delayer.py
@@ -7,7 +7,7 @@ def Delayer(
         timeout,    # Output
         clk,        # Clock
         rst,        # Reset
-        interval,   # Parameter
+        interval,   # Parameter (Seconds)
         clk_freq    # Parameter (Hz)
     ):
     COUNTER_MAX = int(interval * clk_freq)
@@ -17,10 +17,10 @@ def Delayer(
     @always(clk.posedge, rst.posedge)
     def do():
         if rst:
-            counter[:] = 0
+            counter.next = 0
             timeout.next = 0
         else:
-            if counter >= COUNTER_MAX-1:
+            if counter >= COUNTER_MAX:
                 timeout.next = 1
             else:
                 timeout.next = 0
@@ -30,4 +30,37 @@ def Delayer(
 
 
 
+@block
+def DualDelayer(
+        timeout_short,
+        timeout_long,
+        clk,
+        rst,
+        interval_short, # Parameter (Seconds)
+        interval_long,  # Parameter (Seconds)
+        clk_freq, # Parameter (Hz)
+    ):
+    COUNTER_LONG_MAX  = int(interval_long  * clk_freq)
+    COUNTER_SHORT_MAX = int(interval_short * clk_freq) 
+
+    counter = Signal(intbv(0, min=0, max=COUNTER_LONG_MAX+1))
+
+    @always(clk.posedge, rst.posedge)
+    def do():
+        if rst:
+            counter.next = 0
+            timeout_short.next = 0
+            timeout_long.next = 0
+        else:
+            if counter >= COUNTER_SHORT_MAX:
+                timeout_short.next = 1
+            else:
+                timeout_short.next = 0
+            if counter >= COUNTER_LONG_MAX:
+                timeout_long.next = 1
+            else:
+                timeout_long.next = 0
+                counter.next = counter + 1
+
+    return instances()
 
--- wavesynlib/cores/pydht11.py
+++ wavesynlib/cores/pydht11.py
@@ -6,7 +6,7 @@ from myhdl import (
     ConcatSignal,
     always_seq, always_comb, always, instances)
 
-from wavesynlib.cores.delayer import Delayer
+from wavesynlib.cores.delayer import Delayer, DualDelayer
 
 
 
@@ -47,12 +47,18 @@ def PyDHT11(
             state_out.next = state
     
     timeout_20ms = Signal(bool(0))
-    rst_20ms = Signal(bool(0))
-    delay20ms = Delayer(timeout_20ms, clk, rst_20ms, 20e-3, clk_freq)
-
     timeout_40us = Signal(bool(0))
-    rst_40us = Signal(bool(0))
-    delay40us = Delayer(timeout_40us, clk, rst_40us, 40e-6, clk_freq)
+    delayer_rst = Signal(bool(0))
+
+    delayer = DualDelayer(
+        timeout_short=timeout_40us, 
+        timeout_long=timeout_20ms,
+        clk=clk,
+        rst=delayer_rst,
+        interval_short=40e-6,
+        interval_long=20e-3,
+        clk_freq=clk_freq)
+
 
     shift_reg = [Signal(bool(0)) for k in range(40)]
     bit_counter = Signal(intbv(0, min=0, max=41))
@@ -67,21 +73,21 @@ def PyDHT11(
                 inout_state.next = READ_LINE
             else:
                 bit_counter.next = 0
-                rst_20ms.next = 1
+                delayer_rst.next = 1
                 inout_state.next = WRITE_LINE
                 valid.next = 0
                 state.next = States.START_SIGNAL_LOW
         elif state == States.START_SIGNAL_LOW:
             if not timeout_20ms:
-                rst_20ms.next = 0
+                delayer_rst.next = 0
                 line_out.next = 0
             else:
-                rst_40us.next = 1
+                delayer_rst.next = 1
                 inout_state.next = READ_LINE
                 state.next = States.START_SIGNAL_HIGH
         elif state == States.START_SIGNAL_HIGH:
             if not timeout_40us:
-                rst_40us.next = 0
+                delayer_rst.next = 0
             else:
                 state.next = States.WAIT_RESPONSE_LOW
         elif state == States.WAIT_RESPONSE_LOW:
@@ -95,11 +101,11 @@ def PyDHT11(
                 state.next = States.WAIT_BIT_HIGH
         elif state == States.WAIT_BIT_HIGH:
             if line_in == 1:
-                rst_40us.next = 1
+                delayer_rst.next = 1
                 state.next = States.WAIT_BIT_DAT
         elif state == States.WAIT_BIT_DAT:
             if not timeout_40us:
-                rst_40us.next = 0
+                delayer_rst.next = 0
             else:
                 for k in range(39):
                     shift_reg[k].next = shift_reg[k+1]
