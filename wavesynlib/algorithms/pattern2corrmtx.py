2153daec807c7db0d1defb5863d3e3817272ac7c
xialulee <xialulee@sina.com>
2019-06-09 15:25:34 +0800

    Update pattern2corrmtx.py

--- wavesynlib/algorithms/pattern2corrmtx.py
+++ wavesynlib/algorithms/pattern2corrmtx.py
@@ -5,9 +5,9 @@ Created on Wed Apr 30 22:19:30 2014
 @author: Tao Zhang, Feng-cong Li (xialulee@sina.com)
 """
 
-from numpy import exp, kron, mat, pi, r_, real, sin, vstack, zeros
+from numpy import exp, kron, mat, pi, r_, real, sin, vstack, zeros, ones, diag
 import cvxpy as cp
-import collections
+from collections.abc import Iterable
 from abc import ABCMeta, abstractproperty
 
 class BasePattern(metaclass=ABCMeta):
@@ -63,7 +63,7 @@ def matJ(M):
     addr_table  = {}
     for q in range(M):
         for p in range(q, M):
-            addr_table[(p,q)]   = k
+            addr_table[(p,q)] = k
             k += 1 if p==q else 2
     
     for q in range(M):
@@ -89,16 +89,16 @@ def matA(M, angles):
 
     
 def matG(M, angles, magnitude):
-    G1  = mat(zeros((M**2+1, M**2+1)))
-    A   = mat(matA(M, angles))
-    J   = mat(matJ(M))
+    G1  = zeros((M**2+1, M**2+1))
+    A   = matA(M, angles)
+    J   = matJ(M)
     for index, theta in enumerate(angles):
-        g   = -(kron(A[:,index].T, A[:,index].H) * J).T
-        t   = mat(vstack((magnitude[index], g)));
-        G1  += real(t * t.T)
+        g   = -(kron(A[:,index].T, A[:,index].H) @ J).T
+        t   = vstack((magnitude[index], g))
+        G1  += real(t @ t.T)
     G1  = 1.0/len(angles) * G1
     G1  = real(G1)
-    return mat(G1)
+    return G1
     
                 
               
@@ -136,8 +136,8 @@ class Problem(object):
     def idealPattern(self, val):
         if not isinstance(val, BasePattern):
             raise TypeError('idealPattern should assign to an instance of a derived class of BasePattern.')
-        if (not isinstance(val.samplesAngle, collections.Iterable)) or\
-                (not isinstance(val.samplesMagnitude, collections.Iterable)):
+        if (not isinstance(val.samplesAngle, Iterable)) or\
+                (not isinstance(val.samplesMagnitude, Iterable)):
             raise TypeError('angle and magnitude of idealPattern should be iterable.')
         if len(val.samplesAngle) != len(val.samplesMagnitude):
             raise Exception('len(idealPattern.angle) should equal len(idealPattern.magnitude).')
@@ -147,52 +147,29 @@ class Problem(object):
             
     def solve(self, *args, **kwargs):
         self.__setup()
-        self.__problem.solve(*args, **kwargs)
-        R   = (self.__ReR.value + 1j * self.__ImR.value)
-        return R        
+        self.__problem.solve(*args, **kwargs)  
+        return self.__R.value
         
 
     def __setup(self):  
-        M   = self.__M           
-        Rreal = cp.semidefinite(2*M, 'Rreal')
-        # Rreal = [B1 B3
-        #          B2 B4 ]
-        #       = [ReR -ImR
-        #          ImR  ReR]    
-        B1 = Rreal[0:M, 0:M]
-        B2 = Rreal[M:(2*M), 0:M]
-        B3 = Rreal[0:M, M:(2*M)]
-        B4 = Rreal[M:(2*M), M:(2*M)]
-        rho = cp.Variable(1+M**2, 1, 'rho')
-
-        constraints = [] # the list of the constraint equations and inequations
-        constraints.append(0 <= rho[0])
-        k   = 1
-        for q in range(M):
-            for p in range(M):
-                if p == q:
-                    constraints.append(rho[k] == B1[p, q])
-                    k += 1
-                elif p > q:
-                    constraints.append(rho[k] == B1[p, q])
-                    k += 1
-                    constraints.append(rho[k] == B2[p, q])
-                    k += 1
-            
-        constraints.append(B1 == B4)
-        constraints.append(B1 == B1.T)
-        constraints.append(B3 == -B2)
-        constraints.append(B3 == -B3.T)
+        M = self.__M     
+        J = matJ(M)
+        R = cp.Variable((M, M), hermitian=True)
+        rho = cp.Variable((1+M**2,))
+        alpha = rho[0]
+        r = rho[1:]
+                
+        objective = cp.Minimize(cp.quad_form(rho, self.__Gamma))
         
-        constraints.extend([B1[k,k]==1 for k in range(M)])
-
-
-        objective   = cp.Minimize(cp.quad_form(rho, self.__Gamma))
-
-        problem     = cp.Problem(objective, constraints)
+        constraints = [
+            R >> 0,
+            alpha >= 0,
+            cp.vec(R) == J@r,
+            cp.diag(cp.real(R)) == 1]         
+        
+        problem = cp.Problem(objective, constraints)
         self.__problem  = problem
-        self.__ReR      = B1
-        self.__ImR      = B2
+        self.__R = R
 
 
 
@@ -209,10 +186,10 @@ if __name__ == '__main__':
         problem.M = M
         problem.idealPattern = idealp
         R = problem.solve(verbose=True)    
-        
+        print(diag(R))
         pattern = corrmtx2pattern(R, angles)
         plot(angles, pattern)
-        hold(True)
+#        hold(True)
         plot(angles, idealp.samplesMagnitude, 'r')  
         show()    
     test()
