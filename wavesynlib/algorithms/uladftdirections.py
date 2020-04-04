c93a8a5bbe76918fda5defa10bdf76b92d485744
xialulee <xialulee@live.cn>
2020-04-04 23:46:13 +0800

    Bug fix.

--- wavesynlib/algorithms/uladftdirections.py
+++ wavesynlib/algorithms/uladftdirections.py
@@ -1,5 +1,5 @@
 from math import ceil, pi
-from numpy import abs, arcsin, deg2rad, exp, inner, r_, rad2deg, sin, sum
+from numpy import abs, arcsin, deg2rad, dot, exp, inner, r_, rad2deg, sin, sum
 from pandas import DataFrame
 
 
@@ -29,21 +29,21 @@ d: the space between elements with respect to wavelength."""
 
 
 
-def calc_dft_peak(M, d, theta_deg=None, theta_rad=None):
+def calc_dft_indices(M, d, theta_deg=None, theta_rad=None):
     result = []
-    if theta_deg:
+    if theta_deg is not None:
         theta_rad = deg2rad(theta_deg)
     else:
         theta_deg = rad2deg(theta_rad)
     theta_k = sin(theta_rad) * d * M % M
-    print(theta_k)
     for index, k in enumerate(theta_k):
         s1 = exp(1j*2*pi*k*r_[:M]/M)
         s2 = exp(1j*2*pi*round(k)*r_[:M]/M)
-        loss = 1 - abs(sum(s1*s2.conj()))/16
+        loss = 1 - abs(sum(s1*s2.conj()))/M
         result.append({
             "θ(°)": theta_deg[index],
             "θ(rad)": theta_rad[index],
-            "peak": int(k),
+            "freq": k/M,
+            "k": round(k),
             "loss": loss })
     return DataFrame(result)
