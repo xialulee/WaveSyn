b75d1c56d52232f0bb3352585130975841838f48
xialulee <xialulee@live.cn>
2020-05-15 02:29:04 +0800

    Update packbits.pyx
    Refined implementation.

--- wavesynlib/fileutils/photoshop/packbits.pyx
+++ wavesynlib/fileutils/photoshop/packbits.pyx
@@ -6,35 +6,53 @@ from libc.stdint cimport uint8_t
 
 
 
+@cython.boundscheck(False)
 @cython.wraparound(False)
 def packbits(const uint8_t [::1] buf, int image_size):
-    cdef int buf_size
-    cdef int row_idx
-    cdef int size_pos
-    cdef int size
-    cdef int col
-    cdef int len_data
-    cdef int cnt
-    cdef uint8_t value 
-    cdef np.ndarray[np.uint8_t, ndim=1] pixels 
+    cdef: 
+        int buf_size
+        int row_idx
+        int size_pos
+        int size
+        int col
+        int len_data
+        int cnt
+        uint8_t value 
+        np.ndarray[np.uint8_t, ndim=1] pixels 
+        uint8_t [::1] pixel_view
+
     
-    pixels = np.zeros([image_size], dtype=np.uint8)
+    pixels = np.empty([image_size], dtype=np.uint8)
+    # The continuous of pixels is guaranteed.
+    # Use the continous view to avoid unnecessary multiplication 
+    # to stride of indexing. 
+    pixel_view = pixels
+
     buf_size = len(buf)
     col = 0
     size_pos = 0
     
-    while size_pos < buf_size:
+    while size_pos < buf_size-1: 
+        # size_pos cannot be the last byte of buf.
+        # hence size_pos < buf_size MINUS ONE.
         size = buf[size_pos]
         if size >= 128:
             size = 257 - size
             value = buf[size_pos+1]
+            # bounds check for pixel_view
+            if col+size > image_size:
+                raise IndexError("Pixel array index out of range.")
             for cnt in range(size):
-                pixels[col+cnt] = value
+                pixel_view[col+cnt] = value
             size_pos += 2
         else:
             size += 1
+            if col+size > image_size:
+                raise IndexError("Pixel array index out of range.")
+            if size_pos+1+size > buf_size:
+                raise IndexError("RLE data buf index out of range.")
             for cnt in range(size):
-                pixels[col+cnt] = buf[size_pos+1+cnt]
+                pixel_view[col+cnt] = buf[size_pos+1+cnt]
             size_pos += size + 1
 
         col += size
