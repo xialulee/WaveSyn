41106438bf2b387703b2b1d78654778991b0c431
xialulee <xialulee@live.cn>
2020-09-16 00:05:28 +0800

    Update wsl.py
    Bug fix:
    Now winpath_to_wslpath can handle path like "//wsl$/Ubuntu/home".

--- wavesynlib/interfaces/os/windows/wsl.py
+++ wavesynlib/interfaces/os/windows/wsl.py
@@ -3,8 +3,11 @@ from pathlib import Path
 
 
 def winpath_to_wslpath(path):
-    result = Path("/mnt")
-    result /= path.drive[:-1].lower()
+    if str(path).startswith(r"\\wsl$"):
+        result = Path("/")
+    else:
+        result = Path("/mnt")
+        result /= path.drive[:-1].lower()
     for part in path.parts[1:]:
         result /= part.lower()
     return result
