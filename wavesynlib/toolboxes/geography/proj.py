1ea6ddeabc166bc8e5d987a726eb41b13a2620fd
xialulee <xialulee@live.cn>
2020-11-25 18:47:25 +0800

    Update proj.py
    New function wgs84_to_enu for converting WGS84 coords to ENU coords.

--- wavesynlib/toolboxes/geography/proj.py
+++ wavesynlib/toolboxes/geography/proj.py
@@ -22,8 +22,8 @@ def wgs84_to_lla(x, y, z):
     x, y, z = [arg.rescale(pq.meter) if isinstance(arg, pq.Quantity) else arg
                     for arg in (x, y, z)]
     lon, lat, alt = _wgs84_to_lla_transformer.transform(x, y, z)
-    data = np.hstack((lon, lat, alt))
-    data = data.reshape(-1, 3)
+    data = np.vstack((lon, lat, alt))
+    data = data.transpose()
     head = ["longitude/deg", "latitude/deg", "altitude/m"]
     return DataFrame(data=data, columns=head)
 
@@ -38,3 +38,35 @@ def lla_to_wgs84(lon, lat, alt):
     data = data.reshape(-1, 3)
     head = ("x/m", "y/m", "z/m")
     return DataFrame(columns=head, data=data)
+
+
+
+def wgs84_to_enu(x, y, z, x0, y0, z0, ell=None):
+    lla0 = wgs84_to_lla(x0, y0, z0)
+    lon0 = lla0.iloc[0]['longitude/deg']
+    lat0 = lla0.iloc[0]['latitude/deg']
+    alt0 = lla0.iloc[0]['altitude/m']
+
+    lla = wgs84_to_lla(x, y, z)
+    lon = lla['longitude/deg'].to_numpy()
+    lat = lla['latitude/deg'].to_numpy()
+    alt = lla['altitude/m'].to_numpy()
+
+    east, north, up = pymap3d.enu.geodetic2enu(lat=lat, lon=lon, h=alt, lat0=lat0, lon0=lon0, h0=alt0)
+    data = np.vstack((east, north, up))
+    data = data.transpose()
+    head = ("east/m", "north/m", "altitude/m")
+    return DataFrame(data=data, columns=head)
+
+
+
+if __name__ == "__main__":
+    pass
+
+
+
+
+
+
+
+
