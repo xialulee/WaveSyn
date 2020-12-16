9d1e2caff7809a2028e9941d3e17ef57bedbefba
xialulee <xialulee@live.cn>
2020-12-16 13:49:13 +0800

    Update pattern.py
    Define patterns in the module.

--- wavesynlib/languagecenter/python/pattern.py
+++ wavesynlib/languagecenter/python/pattern.py
@@ -11,30 +11,37 @@ import re
 
 
 
-def any(name, alternates):
+
+
+
+def any_(name, alternates):
     "Return a named group pattern matching list of alternates."
     # Copied from idlelib.colorizer
     return "(?P<%s>" % name + "|".join(alternates) + ")"
 
 
+
+kw = r"\b" + any_("KEYWORD", keyword.kwlist) + r"\b"
+builtinlist = [str(name) for name in dir(builtins)
+                                    if not name.startswith('_') and \
+                                    name not in keyword.kwlist]
+# self.file = open("file") :
+# 1st 'file' colorized normal, 2nd as builtin, 3rd as string
+builtin = r"([^.'\"\\#]\b|^)" + any_("BUILTIN", builtinlist) + r"\b"
+comment = any_("COMMENT", [r"#[^\n]*"])
+stringprefix = r"(?i:\br|u|f|fr|rf|b|br|rb)?"
+sqstring = stringprefix + r"'[^'\\\n]*(\\.[^'\\\n]*)*'?"
+dqstring = stringprefix + r'"[^"\\\n]*(\\.[^"\\\n]*)*"?'
+sq3string = stringprefix + r"'''[^'\\]*((\\.|'(?!''))[^'\\]*)*(''')?"
+dq3string = stringprefix + r'"""[^"\\]*((\\.|"(?!""))[^"\\]*)*(""")?'
+string = any_("STRING", [sq3string, dq3string, sqstring, dqstring])
+
+
+
 def make_pat():
     # Copied from idlelib.colorizer
-    kw = r"\b" + any("KEYWORD", keyword.kwlist) + r"\b"
-    builtinlist = [str(name) for name in dir(builtins)
-                                        if not name.startswith('_') and \
-                                        name not in keyword.kwlist]
-    # self.file = open("file") :
-    # 1st 'file' colorized normal, 2nd as builtin, 3rd as string
-    builtin = r"([^.'\"\\#]\b|^)" + any("BUILTIN", builtinlist) + r"\b"
-    comment = any("COMMENT", [r"#[^\n]*"])
-    stringprefix = r"(?i:\br|u|f|fr|rf|b|br|rb)?"
-    sqstring = stringprefix + r"'[^'\\\n]*(\\.[^'\\\n]*)*'?"
-    dqstring = stringprefix + r'"[^"\\\n]*(\\.[^"\\\n]*)*"?'
-    sq3string = stringprefix + r"'''[^'\\]*((\\.|'(?!''))[^'\\]*)*(''')?"
-    dq3string = stringprefix + r'"""[^"\\]*((\\.|"(?!""))[^"\\]*)*(""")?'
-    string = any("STRING", [sq3string, dq3string, sqstring, dqstring])
     return kw + "|" + builtin + "|" + comment + "|" + string +\
-           "|" + any("SYNC", [r"\n"])
+           "|" + any_("SYNC", [r"\n"])
            
            
 
