32403b8489c595b2dfdb93fd16cd62961cfd873e
xialulee <xialulee@live.cn>
2020-12-23 15:42:55 +0800

    Update wswslpath.py
    1. Rename wsuowpath to wswslpath;
    2. support convert relative path on windows;
    3. convert \r\n to \n of the source file.

--- wavesynlib/gadgets/wswslpath.py
+++ wavesynlib/gadgets/wswslpath.py
@@ -7,38 +7,46 @@ import argparse
 import pathlib
 from functools import reduce
 from operator import truediv
+import platform
 
 
 
 def _driver_convert(driver):
-    name = driver[:driver.find(":")]
+    name = driver[:driver.find(":")].lower()
     return pathlib.PurePosixPath(f"/mnt/{name}")
 
 
 
-def _w2u(path):
+
+def _w2l(path):
     path = pathlib.PureWindowsPath(path)
     if path.is_absolute():
         parts = path.parts
         p0 = _driver_convert(parts[0])
         p1 = parts[1:]
     else:
-        # To-Do: This code only supports running on Linux.
-        # Please add Windows support.
         p0 = pathlib.Path(".").absolute()
         p1 = path.parts
+        if platform.system() == "Windows":
+            pa = p0.parts
+            p0 = _driver_convert(pa[0])
+            p1 = [*pa[1:], *p1]
     return str(reduce(truediv, [p0, *p1]))
 
 
 
 def main(args):
     parser = argparse.ArgumentParser(
-        description="Convert Windows path to UoW path.")
+        description="Convert Windows path to wsl linux path.")
     parser.add_argument('path', metavar='path', type=str, help='The given path.')
     args = parser.parse_args(args)
     path = args.path
     path = pathlib.PureWindowsPath(path)
-    print(_w2u(path))
+    try:
+        print(_w2l(path))
+    except TypeError:
+        print("OS not supported.", file=sys.stderr)
+        sys.exit(1)
 
 
 
