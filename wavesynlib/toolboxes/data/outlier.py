bc8f0cec5b07048dbae2b1edbfbcd57791e524a9
xialulee <xialulee@live.cn>
2021-04-30 21:56:56 +0800

    Make a new toolbox for processing data.

---
+++ wavesynlib/toolboxes/data/outlier.py
@@ -0,0 +1,5 @@
+from numpy import all, r_, median, var, sqrt, hstack, split
+from numpy.linalg import norm
+
+
+
---
+++ wavesynlib/toolboxes/data/toolboxnode.py
@@ -0,0 +1,8 @@
+
+from ..basetoolboxnode import BaseToolboxNode
+
+
+
+class ToolboxNode(BaseToolboxNode):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
