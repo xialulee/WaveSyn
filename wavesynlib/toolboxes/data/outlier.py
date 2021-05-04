035425c3dcaaf9ffeb6d7b1524b6e454ef0611ac
xialulee <xialulee@live.cn>
2021-05-04 17:20:26 +0800

    Update outlier.py
    Implement an outlier detector using DBSCAN.

--- wavesynlib/toolboxes/data/outlier.py
+++ wavesynlib/toolboxes/data/outlier.py
@@ -1,5 +1,18 @@
-from numpy import all, r_, median, var, sqrt, hstack, split
-from numpy.linalg import norm
+from numpy import empty
+from sklearn.cluster import DBSCAN
 
 
 
+def dbscan_detect_outliers(*args, **kwargs):
+    len_args = len(args)
+    samples = None
+    if len_args == 0:
+        raise ValueError("No sample input.")
+    elif len_args == 1:
+        samples = args[0]
+    else:
+        samples = empty((len(args[0]), len_args))
+        for i in range(len_args):
+            samples[:, i] = args[i]
+    clustering = DBSCAN(**kwargs).fit(samples)
+    return clustering.labels_ == -1
