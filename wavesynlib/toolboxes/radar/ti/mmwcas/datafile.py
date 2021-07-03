5a56963909cdfb83fdfb2fc290f4b019c53cfaae
xialulee <xialulee@live.cn>
2021-07-03 23:38:46 +0800

    Update datafile.py

--- wavesynlib/toolboxes/radar/ti/mmwcas/datafile.py
+++ wavesynlib/toolboxes/radar/ti/mmwcas/datafile.py
@@ -14,7 +14,7 @@ class Config:
     idle_time:          pq.Quantity = None
     adc_start_time:     pq.Quantity = None
     ramp_end_time:      pq.Quantity = None
-    sampling_rate:      int = None
+    sampling_rate:      pq.Quantity = None
     sample_per_chirp:   int = None
     num_loops:          int = None
     num_frames:         int = None
@@ -24,6 +24,7 @@ class Config:
     sample_per_channel: int = field(init=False)
     center_freq:        pq.Quantity = field(init=False)
     angle_per_sweep:    int = field(init=False)
+    range_resolution:   pq.Quantity = field(init=False)
 
 
     def __post_init__(self):
@@ -31,8 +32,8 @@ class Config:
         self.angle_per_sweep    = self.angles_to_steer.size
         # !
         self.sample_per_channel = self.sample_per_chirp * self.num_loops * self.angle_per_sweep * self.num_frames
-        self.center_freq        = (self.start_freq + self.sample_per_chirp/self.sampling_rate*pq.second*self.chirp_slope/2).rescale(pq.GHz)
-
+        self.center_freq        = (self.start_freq + self.sample_per_chirp/self.sampling_rate*self.chirp_slope/2).rescale(pq.GHz)
+        self.range_resolution   = (3e8*pq.m/pq.s / 2 / (self.sample_per_chirp / self.sampling_rate * self.chirp_slope)).rescale(pq.m)
 
 
 
@@ -49,7 +50,7 @@ def read_mmwave_json(path):
     kwargs["idle_time"]        = rl_profiles["idleTimeConst_usec"] * pq.us
     kwargs["adc_start_time"]   = rl_profiles["adcStartTimeConst_usec"] * pq.us
     kwargs["ramp_end_time"]    = rl_profiles["rampEndTime_usec"] * pq.us
-    kwargs["sampling_rate"]    = rl_profiles["digOutSampleRate"] * 1000 # Unit of sampling rate in json is ksps
+    kwargs["sampling_rate"]    = rl_profiles["digOutSampleRate"] * pq.kHz # Unit of sampling rate in json is ksps
     kwargs["sample_per_chirp"] = rl_profiles["numAdcSamples"]
     kwargs["num_loops"]        = frame_seq['subFrameCfg'][0]['rlSubFrameCfg_t']['numLoops']
     kwargs["num_frames"]       = frame_seq["numFrames"]
@@ -75,9 +76,9 @@ def read_bin_file(path, frame_index, sample_per_chirp, chirp_per_loop, loop_per_
 
 if __name__ == '__main__':
     # Test
-    path = r"C:\LocalWork\Cascade\20210626\20_29_53_06_26_21\slave1_0000_data.bin"
+    path = r"D:\LocalWork\Cascade\20210626\20_29_53_06_26_21\slave1_0000_data.bin"
     cube = read_bin_file(path, 2, 256, 64, 31, 4)
 
-    path = r"C:\LocalWork\Cascade\20210626\20_29_53_06_26_21\20_29_53_06_26_21.mmwave.json"
+    path = r"D:\LocalWork\Cascade\20210626\20_29_53_06_26_21\20_29_53_06_26_21.mmwave.json"
     config = read_mmwave_json(path)
     print(config)
