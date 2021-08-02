0184784d2c3279bc28c88aa3af936bf8617267f2
xialulee <xialulee@live.cn>
2021-08-02 20:32:19 +0800

    New add_lazy_local_node method of toolboxnode.

--- wavesynlib/toolboxes/basetoolboxnode.py
+++ wavesynlib/toolboxes/basetoolboxnode.py
@@ -17,3 +17,15 @@ class BaseToolboxNode(ModelNode):
     @property
     def toolbox_package_path(self):
         return inspect.getmodule(self).__name__.rsplit('.', 1)[0]
+
+
+    def add_lazy_local_node(self, node_name, module_name, class_name):
+        if not module_name.startswith("."):
+            module_name = f".{module_name}"
+        setattr(
+            self, 
+            node_name, 
+            ModelNode(
+                is_lazy=True,
+                module_name=f"{self.toolbox_package_path}{module_name}",
+                class_name=class_name))
--- wavesynlib/toolboxes/radar/toolboxnode.py
+++ wavesynlib/toolboxes/radar/toolboxnode.py
@@ -6,11 +6,7 @@ from ..basetoolboxnode import BaseToolboxNode
 class ToolboxNode(BaseToolboxNode):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
-        self.band_info = ModelNode(
-            is_lazy = True,
-            module_name=f"{self.toolbox_package_path}.bands.modelnode",
-            class_name="BandInfo")
-
-
-    
+        self.add_lazy_local_node(
+            "band_info", 
+            ".bands.modelnode", 
+            "BandInfo")
