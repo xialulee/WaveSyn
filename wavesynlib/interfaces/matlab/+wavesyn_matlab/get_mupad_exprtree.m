9822d48ea6e9c798df70ce2da2bd3224e9f0fc59
xialulee <xialulee@live.cn>
2022-03-26 19:17:39 +0800

    Adjust indent.

--- wavesynlib/interfaces/matlab/+wavesyn_matlab/get_mupad_exprtree.m
+++ wavesynlib/interfaces/matlab/+wavesyn_matlab/get_mupad_exprtree.m
@@ -1,5 +1,5 @@
 function [tree, var] = get_mupad_exprtree(expr)
-tree = evalin(symengine, sprintf('prog::exprlist(%s)', char(expr)));
-tree = evalc('disp(tree)');
-var  = evalc('disp(symvar(expr))');
+    tree = evalin(symengine, sprintf('prog::exprlist(%s)', char(expr)));
+    tree = evalc('disp(tree)');
+    var  = evalc('disp(symvar(expr))');
 end
--- wavesynlib/interfaces/matlab/+wavesyn_matlab/get_process_id.m
+++ wavesynlib/interfaces/matlab/+wavesyn_matlab/get_process_id.m
@@ -1,5 +1,5 @@
 function pid = get_process_id()
-import System.Diagnostics.*
-process = System.Diagnostics.Process.GetCurrentProcess();
-pid = process.Id;
+    import System.Diagnostics.*
+    process = System.Diagnostics.Process.GetCurrentProcess();
+    pid = process.Id;
 end
