c7c3da3dbac09761427b53655f0d88448b2d51af
xialulee <xialulee@live.cn>
2022-11-15 13:57:14 +0800

    Reimplement hyhook.hy in Python.
    wavesynlib.languagecenter.datatypes.wintypes.hyhook.hy -> hook.py.

--- wavesynlib/languagecenter/datatypes/wintypes/hook.py
+++ wavesynlib/languagecenter/datatypes/wintypes/hook.py
@@ -1,2 +1,16 @@
-import hy
-from .hyhook import *
+from ctypes import Structure, WINFUNCTYPE, POINTER, c_int
+from ctypes.wintypes import WPARAM, DWORD
+from .ptrinteger import ULONG_PTR, LRESULT
+
+
+class KBDLLHOOKSTRUCT(Structure):
+    _fields_ = [
+        ('vkCode', DWORD), 
+        ('scanCode', DWORD), 
+        ('flags', DWORD), 
+        ('time', DWORD), 
+        ('dwExtraInfo', ULONG_PTR)
+    ]
+
+
+KHOOKPROC = WINFUNCTYPE(LRESULT, c_int, WPARAM, POINTER(KBDLLHOOKSTRUCT))
