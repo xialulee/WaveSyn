9b0d116718f60c4ccb8b260ac83ebaf6b2cec0ea
xialulee <xialulee@live.cn>
2022-11-15 21:03:02 +0800

    Reimplement hynode.hy in Python.
    wavesynlib.interfaces.hynode.hy -> modelnode.py.

--- wavesynlib/interfaces/modelnode.py
+++ wavesynlib/interfaces/modelnode.py
@@ -1,9 +1,36 @@
-import os
-from os.path import dirname, join
-import hy
-try:
-    from wavesynlib.interfaces.hynode import *
-except hy.errors.HyCompileError:
-    node_path = join(dirname(__file__), 'hynode.hy')
-    os.system(f'hyc {node_path}')
-    from wavesynlib.interfaces.hynode import *
+from wavesynlib.languagecenter.wavesynscript import ModelNode
+from wavesynlib.interfaces.editor.modelnode import EditorDict
+
+
+class Interfaces(ModelNode):
+    """The interfaces node of WaveSyn, which provides several mechanisms for
+communicating with different software applications and hardware devices."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.os = ModelNode(
+            is_lazy=True, 
+            module_name='wavesynlib.interfaces.os.modelnode',
+            class_name='OperatingSystem'
+        )
+        self.net = ModelNode(
+            is_lazy=True, 
+            module_name = 'wavesynlib.interfaces.net.modelnode', 
+            class_name ='Net'
+        )
+        self.gpu = ModelNode(
+            is_lazy=True, 
+            module_name='wavesynlib.interfaces.gpu',
+            class_name='GPU'
+        )
+        self.dotnet = ModelNode(
+            is_lazy=True, 
+            module_name= 'wavesynlib.interfaces.dotnet', 
+            class_name = 'DotNet'
+        )
+        self.imagemagick = ModelNode(
+            is_lazy=True, 
+            module_name= 'wavesynlib.interfaces.imagemagick', 
+            class_name='ImageMagickNode'
+        )
+        self.editors = EditorDict()
