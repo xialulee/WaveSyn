670dcf157142a10fa8d46889d292d11283ecbcac
xialulee <xialulee@live.cn>
2022-11-15 15:44:56 +0800

    Reimplement hymemstatus.hy in Python.
    wavesynlib.interfaces.os.windows.hymemstatus.hy -> memstatus.py.

--- wavesynlib/interfaces/os/windows/memstatus.py
+++ wavesynlib/interfaces/os/windows/memstatus.py
@@ -4,21 +4,30 @@ Created on Thur Feb 25 2016
 
 @author: Feng-cong Li
 """
-import ctypes as ct
-import os
-from pathlib import Path
-# The following code generates the bytecode file of the 
-# memstatus_struct.hy which is written in Hy.
-# If we import a module written in hy directly in wavesyn,
-# it will fail, and I cannot figure out why. 
-import hy
-try:
-    from wavesynlib.interfaces.os.windows.hymemstatus import *
-except hy.errors.HyCompileError:
-# After the bytecode file generated, we can import the module written by hy.    
-    hyfile_path = Path(__file__).parent / 'hymemstatus.hy'
-    os.system(f'hyc {hyfile_path}')
-    from wavesynlib.interfaces.os.windows.hymemstatus import *
+from ctypes import Structure
+from ctypes.wintypes import DWORD
+from ctypes import c_size_t as size_t
+from ctypes import sizeof, byref
+from ctypes import windll
+GlobalMemoryStatus = windll.kernel32.GlobalMemoryStatus
 
 
+class MEMORYSTATUS(Structure):
+    _fields_ = [
+        ("dwLength", DWORD), 
+        ("dwMemoryLoad", DWORD), 
+        ("dwTotalPhys", size_t), 
+        ("dwAvailPhys", size_t), 
+        ("dwTotalPageFile", size_t), 
+        ("dwAvailPageFile", size_t), 
+        ("dwTotalVirtual", size_t), 
+        ("dwAvailVirtual", size_t)
+    ]
+
+
+def get_memory_usage():
+    memstat = MEMORYSTATUS()
+    memstat.dwLength = sizeof(MEMORYSTATUS)
+    GlobalMemoryStatus(byref(memstat))
+    return memstat.dwMemoryLoad
 
