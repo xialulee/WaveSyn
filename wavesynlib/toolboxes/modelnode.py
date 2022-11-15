13c5f2740747be4cd06a37600ff88934b74a14d7
xialulee <xialulee@live.cn>
2022-11-15 21:37:09 +0800

    Reimplement hynode.hy in Python.
    wavesynlib.toolboxes.hynode.hy -> modelnode.py.

--- wavesynlib/toolboxes/modelnode.py
+++ wavesynlib/toolboxes/modelnode.py
@@ -1,2 +1,26 @@
-import hy
-from .hynode import Toolboxes
+from importlib import import_module
+from wavesynlib.languagecenter.wavesynscript import NodeDict
+
+
+
+class Toolboxes(NodeDict):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def __getitem__(self, toolbox_name):
+        if toolbox_name in self:
+            return super().__getitem__(toolbox_name)
+        mod = import_module(
+            f'.{toolbox_name}.toolboxnode', '.'.join(
+                __name__.split('.')[0:-1:None]
+            )
+        )
+        node = mod.ToolboxNode(toolbox_name=toolbox_name)
+        self[toolbox_name] = node
+        return node
+
+    def _make_child_path(self, child):
+        return f'{self.node_path}[{repr(child._toolbox_name)}]'
+
+    def _hy_make_child_path(self, child):
+        return f'{self.hy_node_path[0:-1:None]} [{repr(child._toolbox_name)}])'
