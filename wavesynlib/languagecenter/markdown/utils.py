f64b20ee1b6fe97d5d66d245a6a4f8a8c34003ff
xialulee <xialulee@live.cn>
2022-11-21 21:01:09 +0800

    Update utils.py
    Remove unnecessary code.

--- wavesynlib/languagecenter/markdown/utils.py
+++ wavesynlib/languagecenter/markdown/utils.py
@@ -6,7 +6,6 @@ from typing import List
 def table_to_code(table: List[List], head: List|None = None) -> str:
     if not head:
         head, table = table[0], table[1:]
-        # table = table[1:]
     head_code = '|'.join(map(str, head))
     split_code = '|'.join(['---'] * len(head))
     rows = [head_code, split_code]
