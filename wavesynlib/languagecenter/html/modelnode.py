4fcee079e10d44416bf47e91f53eacf4bf078b6b
xialulee <xialulee@live.cn>
2022-11-23 16:06:57 +0800

    Remove unnecessary code.

--- wavesynlib/languagecenter/html/modelnode.py
+++ wavesynlib/languagecenter/html/modelnode.py
@@ -9,19 +9,6 @@ from pathlib import Path
 
 from wavesynlib.languagecenter.wavesynscript import Scripting, WaveSynScriptAPI, ModelNode
 
-# The following code generates the bytecode file of the 
-# utils.hy which is written in Hy.
-# If we import a module written in hy directly in wavesyn,
-# it will fail, and I cannot figure out why. 
-import hy
-try:
-    from wavesynlib.languagecenter.html import utils
-except hy.errors.HyCompileError:
-# After the bytecode file generated, we can import the module written by hy.    
-    utils_path = Path(__file__).parent / 'utils.hy'
-    os.system(f'hyc {utils_path}')
-    from wavesynlib.languagecenter.html import utils
-    
 
 
 class Utils(ModelNode):
@@ -47,27 +34,27 @@ class Utils(ModelNode):
         return html_code
     
         
-    @WaveSynScriptAPI
-    def get_tables(self, html_code=None, stream=None, file_path=None, encoding=None, strip_cells=False):
-        '''\
-Translate <table>s in HTML code into Python nested lists.
-On Windows platform, it can also retrive tables in clipboard, since MSOffice
-put tables in clipboard using CF_HTML format.
-
-html_code: the HTML code as a string (support CLIPBOARD_HTML const if the
-        clipboard of the OS has the HTML mode).
-    Default: None.
-stream: if html_code is None and stream provided, the HTML code will be 
-        obtained from this stream.
-    Default: None.
-file_path: if html_code and stream are None and file_path provided,
-    the HTML code will be obtained by reading the provided file.
-    Default: None.
-encoding: the encoding of the HTML code. '''
-        html_code = self._get_html_code(html_code, stream, file_path, encoding)
-        return utils.get_table_text(html_code, strip=strip_cells)  
-    
-    
-    @WaveSynScriptAPI
-    def iterable_to_table(self, iterable, have_head=False):
-        return utils.iterable_to_table(iterable, have_head)
+#    @WaveSynScriptAPI
+#    def get_tables(self, html_code=None, stream=None, file_path=None, encoding=None, strip_cells=False):
+#        '''\
+#Translate <table>s in HTML code into Python nested lists.
+#On Windows platform, it can also retrive tables in clipboard, since MSOffice
+#put tables in clipboard using CF_HTML format.
+#
+#html_code: the HTML code as a string (support CLIPBOARD_HTML const if the
+#        clipboard of the OS has the HTML mode).
+#    Default: None.
+#stream: if html_code is None and stream provided, the HTML code will be 
+#        obtained from this stream.
+#    Default: None.
+#file_path: if html_code and stream are None and file_path provided,
+#    the HTML code will be obtained by reading the provided file.
+#    Default: None.
+#encoding: the encoding of the HTML code. '''
+#        html_code = self._get_html_code(html_code, stream, file_path, encoding)
+#        return utils.get_table_text(html_code, strip=strip_cells)  
+#    
+#    
+#    @WaveSynScriptAPI
+#    def iterable_to_table(self, iterable, have_head=False):
+#        return utils.iterable_to_table(iterable, have_head)
--- wavesynlib/languagecenter/hy/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue March 31 2015
-
-@author: Feng-cong Li
-"""
-
